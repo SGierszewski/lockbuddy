b281c4109dcfe5af7e0cff897772c58d6b65985c
SGierszewski <stefaniegierszewski@hotmail.com>
2021-05-18 11:57:24 +0200

    :sparkles: Connect to MongoDB

--- src/database.ts
+++ src/database.ts
@@ -1,3 +1,12 @@
+import { MongoClient } from "mongodb";
+
 export const connectDatabase = async (url: string): Promise<void> => {
-  console.log(url);
+  const client = new MongoClient(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  await client.connect();
+  // verifys that we have access to all databases and get a list
+  const databasesList = await client.db().admin().listDatabases();
+  console.log(databasesList);
 };
