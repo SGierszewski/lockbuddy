6c3ac54e0562d0401ee965b9e394a945be3a1909
SGierszewski <stefaniegierszewski@hotmail.com>
2021-05-19 13:42:06 +0200

    :sparkles: Connect to database

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,11 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
+import { connectDatabase } from "./database";
+
+if (process.env.MONGO_URL === undefined) {
+  throw new Error("Missing env MONGO_URL");
+}
 
 const app = express();
 const port = 5000;
@@ -11,6 +18,9 @@ app.post("/api/credentials", (_request, response) => {
   response.send("Add new credential");
 });
 
-app.listen(port, () => {
-  console.log(`Lockbuddy listening at http://localhost:${port}`);
+connectDatabase(process.env.MONGO_URL).then(() => {
+  console.log("Database connected");
+  app.listen(port, () => {
+    console.log(`Lockbuddy listening at http://localhost:${port}`);
+  });
 });
