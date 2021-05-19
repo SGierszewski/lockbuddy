d5d734023dac218800c6e07b77bbc2f9d3f50124
SGierszewski <stefaniegierszewski@hotmail.com>
2021-05-19 11:47:18 +0200

    :sparkles: Implement API
    The API is based on express.

--- src/server.ts
+++ src/server.ts
@@ -1,66 +1,16 @@
-import dotenv from "dotenv";
-import {
-  deleteCredential,
-  saveCredentials,
-  selectCredential,
-} from "./utils/credentials";
-import { askForMainPassword, askForCommand } from "./utils/question";
-import { isMainPasswordValid } from "./utils/validation";
-import CryptoJS from "crypto-js";
-import { connectDatabase, disconnectDatabase } from "./database";
+import express from "express";
 
-dotenv.config();
+const app = express();
+const port = 5000;
 
-//function start() {
+app.get("/api/credentials", (_request, response) => {
+  response.send("All credentials requested");
+});
 
-const start = async () => {
-  if (process.env.MONGO_URL === undefined) {
-    throw new Error("Missing env MONGO_URL");
-  }
+app.post("/api/credentials", (_request, response) => {
+  response.send("Add new credential");
+});
 
-  await connectDatabase(process.env.MONGO_URL);
-  const mainPassword = await askForMainPassword();
-  if (!(await isMainPasswordValid(mainPassword))) {
-    console.log("Is invalid");
-    start();
-    return;
-  }
-  console.log("Is valid");
-
-  const command = await askForCommand();
-
-  switch (command) {
-    case "list":
-      {
-        const selectedCredential = await selectCredential();
-
-        if (selectedCredential) {
-          const cryptPassword = CryptoJS.AES.decrypt(
-            selectedCredential.password,
-            mainPassword
-          );
-          console.log(
-            `${selectedCredential.service}:
-          Username: ${selectedCredential.username}
-          Password: ${cryptPassword.toString(CryptoJS.enc.Utf8)}`
-          );
-        }
-      }
-      break; // there is only one valid case, therefore a break stops the process; if more cases may be valid no break is needed
-    case "add":
-      {
-        await saveCredentials(mainPassword);
-      }
-      break;
-
-    case "delete":
-      {
-        const selectedCredential = await selectCredential();
-        await deleteCredential(selectedCredential);
-      }
-      break;
-  }
-  await disconnectDatabase();
-};
-
-start();
+app.listen(port, () => {
+  console.log(`Lockbuddy listening at http://localhost:${port}`);
+});
